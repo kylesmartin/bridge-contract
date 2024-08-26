34ac2c285c40a72801803769c31c69d0cdc5d605
TuDo1403 <tudo.dev@gmail.com>
2024-08-26 17:13:04 +0700

    script: fix post check and and more custom postcheck for weth unwrapper removal

--- script/20240807-ir-recover/20240807-ir-recover-testnet.s.sol
+++ script/20240807-ir-recover/20240807-ir-recover-testnet.s.sol
@@ -38,7 +38,7 @@ contract Migration__20240807_IR_Recover_Testnet is Migration {
   TNetwork _currNetwork;
   uint256 _prevForkId;
 
-  address private constant SM_GOVERNOR = address(0x0);
+  address private constant SM_GOVERNOR = address(0xd24D87DDc1917165435b306aAC68D99e0F49A3Fa);
   address private _multisigEth = 0x51F6696Ae42C6C40CA9F5955EcA2aaaB1Cefb26e;
   IMainchainBridgeManager private _mainchainBM;
   TransparentUpgradeableProxyV2 private _mainchainBMproxy;
@@ -91,7 +91,7 @@ contract Migration__20240807_IR_Recover_Testnet is Migration {
     Signature[] memory signatures = _proposal.generateSignatures(gvs, Ballot.VoteType.For);
     Ballot.VoteType[] memory _supports = new Ballot.VoteType[](signatures.length);
 
-    vm.broadcast(gvs[0]);
+    vm.broadcast(SM_GOVERNOR);
     IMainchainBridgeManager(_mainchainBM).relayProposal(_proposal, _supports, signatures);
   }
 
@@ -278,6 +278,13 @@ contract Migration__20240807_IR_Recover_Testnet is Migration {
 
     {
       _postCheck_Withdrawable();
+    }
+
+    // Check `WETHUnwrapper` is removed
+    {
+      // Method get removed, so it reverted in fallback with invalid deposit
+      vm.expectRevert(abi.encodeWithSignature("ErrInvalidInfo()"));
+      address(_mainchainGW).staticcall(abi.encodeWithSignature("WETHUnwrapper()"));
     }
   }
 
