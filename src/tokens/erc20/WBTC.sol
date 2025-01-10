150ab55c490cbff0c8037bb83c72f5b2a7276626
TuDo1403 <tudo.dev@gmail.com>
2025-01-10 13:53:38 +0700

    feat(WBTC): new WBTC implementation

--- src/tokens/erc20/WBTC.sol
+++ src/tokens/erc20/WBTC.sol
@@ -4,14 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract WBTC is ERC20PresetMinterPauser {
-  address public immutable GATEWAY_ADDRESS;
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-  constructor(address gateway, address pauser) ERC20PresetMinterPauser("Wrapped Bitcoin", "WBTC") {
-    _setupRole(DEFAULT_ADMIN_ROLE, pauser);
-    _setupRole(PAUSER_ROLE, pauser);
-
-    GATEWAY_ADDRESS = gateway;
-    _setupRole(MINTER_ROLE, gateway);
+  constructor(address admin, address minter, address burner, address pauser) ERC20PresetMinterPauser("Wrapped Bitcoin", "WBTC") {
+    _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    _grantRole(PAUSER_ROLE, pauser);
+    _grantRole(BURNER_ROLE, burner);
+    _grantRole(MINTER_ROLE, minter);
 
     _revokeRole(DEFAULT_ADMIN_ROLE, _msgSender());
     _revokeRole(MINTER_ROLE, _msgSender());
@@ -27,9 +26,7 @@ contract WBTC is ERC20PresetMinterPauser {
 
   function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20PresetMinterPauser) {
     if (to == address(0)) {
-      if (_msgSender() != GATEWAY_ADDRESS) {
-        revert("WBTC: only gateway can burn tokens");
-      }
+      require(hasRole(BURNER_ROLE, _msgSender()), "WBTC: only burner can burn tokens");
     }
 
     super._beforeTokenTransfer(from, to, amount);
